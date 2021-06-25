9a38aefed9fb0f3026c33998be0b4b60bbbcf1b3
jcarvajalu <jcarvajalu@unal.edu.com>
2021-06-25 15:06:40 -0500

    sout

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -4,6 +4,7 @@ public class JavaApplication12 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola x2");
+        System.out.println("xd");
     }
     
 }
